bcee97b1a8f2bbf5a7a471d1bc7e43080c8577ff
asimetra <asimetra404@gmail.com>
2025-10-24 15:26:58 +0300

    add: resources

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Custom implementation of malloc
 
 ## Resources
-https://www.cs.cornell.edu/courses/cs3410/2016fa/slides/15-allocation.pdf
+- https://www.cs.cornell.edu/courses/cs3410/2016fa/slides/15-allocation.pdf
+- Operating Systems - Three Easy Pieces
--- test.c
+++ test.c
@@ -1,12 +1,16 @@
 #include "malloc.h"
 
+typedef struct {
+    char chars[3];
+    int number;
+} MyStruct;
+
+
+#include <stdio.h>
 int main(void)
 {
-    char *str;
-
-    str = malloc(100);
-    if (str == NULL)
-        return (1);
-    free(str);
+    MyStruct *ptr = (MyStruct *)malloc(sizeof(MyStruct) * 5);
+    printf("Sizeof: %d\n", sizeof(MyStruct));
+    free(ptr);
     return (0);
 }
