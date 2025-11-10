f479e26f56e7dc1386c6d7bc6d5c22ef7b95f2a9
asimetra <asimetra404@gmail.com>
2025-11-10 20:27:55 +0300

    feat: mem2chunk and chunk2idx macros

--- malloc.h
+++ malloc.h
@@ -6,12 +6,19 @@
 #include <sys/mman.h>
 #include <stdio.h>
 
+#define IN_USE(x)      ((x) & (1 >> 1))
+#define SELF_MMAPED(x) ((x) & (1 >> 0))
+#define CHUNK_SIZE(x)  ((x) &  ~(size_t)0b11)
+
+#define mem2chunk(size) ((size + 15) & ~15) // (x + maskj) & ~mask
+#define chunk2idx(size) ((size >> 4) - 1)
+
 /*i just yoinked from glibc*/
 #define MMAP_MAX 65536
 #define MMAP_THRESHOLD  (128 * 1024)
 #define MAX_ALIGNMENT 16
 
-typedef struct arena arena_t;
+
 typedef struct chunk;
 
 typedef struct chunk_pair {
@@ -19,22 +26,26 @@ typedef struct chunk_pair {
     struct chunk *next;
 } chunk_pair_t;
 
+typedef struct chunk_header {
+    size_t      size;   // size h | in_use | self_mmaped
+    chunk_t     *prev;
+} header_t;
+
 typedef struct chunk {
-    size_t          size; // size h | in_use | self_mmaped
-    chunk_t         *prev;
+    header_t        header;
     chunk_pair_t    free_link;
 } chunk_t;
 
 /* Chunk Utils */
-chunk_t *new_chunk(arena_t *arena, size_t size);
-chunk_t *find_free_chunk(chunk_t **head);
+// chunk_t *new_chunk(arena_t *arena, size_t size);
+// chunk_t *find_free_chunk(chunk_t **head);
 
 void    prep_chunk(chunk_t **head, chunk_t *chunk);
 void    remove_chunk(chunk_t **head, chunk_t *chunk);
 
 /* Arena Utils */
-arena_t *create_arena(size_t size);
-void    remove_arena(arena_t *arena);
+// arena_t *create_arena(size_t size);
+// void    remove_arena(arena_t *arena);
 
 /* Malloc API */
 void    *malloc(size_t size);
